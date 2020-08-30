bf1732fc7e042bade4c30de0219af3edef837a51
Francis Duya <sincarf8888@gmail.com>
2020-08-30 12:01:43 -0700

    first commit

---
+++ client/build/precache-manifest.1fba6fe740d2995a919ef42fcfdc50ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4da42fab571fc74788b7",
+    "url": "/static/css/main.db6d0546.chunk.css"
+  },
+  {
+    "revision": "4da42fab571fc74788b7",
+    "url": "/static/js/main.61b31baf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c91480f38857732bbe38",
+    "url": "/static/js/2.c0484b8d.chunk.js"
+  },
+  {
+    "revision": "cb0aa183ba3acb5de730e33b81b50e3b",
+    "url": "/index.html"
+  }
+];
